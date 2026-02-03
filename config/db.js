bb9cdc8ac3bcefbf00580ec3c5c399327277d3a6
nithishsriram2005 <nithishssriram218@gmail.com>
2026-02-03 12:18:25 +0530

    railway

--- config/db.js
+++ config/db.js
@@ -1,17 +1,12 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "Kumaran_cards"
-});
+const db = mysql.createConnection(process.env.DATABASE_URL);
 
 db.connect((err) => {
   if (err) {
-    console.log("❌ MySQL connection failed:", err);
+    console.error("❌ MySQL connection failed:", err);
   } else {
-    console.log("✅ MySQL Connected");
+    console.log("✅ MySQL Connected to Railway");
   }
 });
 
